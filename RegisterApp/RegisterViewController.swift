26a8771dbc914dd4c9e8c6f100afe98eaa103ecc
Davi Pereira Neto <davipneto@hotmail.com>
2021-09-15 18:05:11 -0300

    date picker

--- RegisterApp/RegisterViewController.swift
+++ RegisterApp/RegisterViewController.swift
@@ -13,13 +13,63 @@ enum RegisterTextField: Int {
     case telefone = 2
     case address = 3
     case password = 4
+    case dateOfBirth = 5
 }
 
 class RegisterViewController: UIViewController {
     @IBOutlet var textFields: [UITextField]!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    
+    lazy var validator: Validators = {
+        return Validators(minimumSizeForPassword: 6, textFields: textFields)
+    }()
+    let datePickerView = UIDatePicker()
+    let dateFormatter = DateFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDatePicker()
+        setupDateFormatter()
     }
+    
+    private func setupDatePicker() {
+        datePickerView.datePickerMode = .date
+        datePickerView.preferredDatePickerStyle = .wheels
+        datePickerView.addTarget(self, action: #selector(self.insertDateOnTextField), for: .valueChanged)
+        dateOfBirthTextField.inputView = datePickerView
+    }
+    
+    private func setupDateFormatter() {
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        
+        //dateFormatter.dateFormat = "dd/MM/yy"
+    }
+    
+    @objc private func insertDateOnTextField() {
+        let date = datePickerView.date
+        let dateString = dateFormatter.string(from: date)
+        dateOfBirthTextField.text = dateString
+    }
+    
+    @IBAction func register() {
+        let isValid = validator.validateAllFields()
+        if !isValid {
+            print("Verificar campos")
+        }
+    }
+}
 
+extension RegisterViewController: UIPickerViewDataSource,  UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return nil
+    }
 }
--- RegisterApp/Validators.swift
+++ RegisterApp/Validators.swift
@@ -5,7 +5,51 @@
 //  Created by Curitiba01 on 14/09/21.
 //
 
-import Foundation
+import UIKit
+
+class Validators {
+    let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    var minimumSizeForPassword: Int
+    var textFields: [RegisterTextField: UITextField] = [:]
+    
+    init(minimumSizeForPassword: Int, textFields: [UITextField]) {
+        self.minimumSizeForPassword = minimumSizeForPassword
+        for textField in textFields {
+            if let registerTextField = RegisterTextField(rawValue: textField.tag) {
+                self.textFields[registerTextField] = textField
+            }
+        }
+    }
+    
+    func validateAllFields() -> Bool {
+        return
+            validateName(textFields[.name]!.text!) &&
+            validateEmail(textFields[.email]!.text!) &&
+            validatePhone(textFields[.telefone]!.text!) &&
+            validatePassword(textFields[.password]!.text!)
+    }
+    
+    func validateEmail(_ email: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return predicate.evaluate(with: email)
+    }
+    
+    func validatePassword(_ pass: String) -> Bool {
+        return pass.count >= minimumSizeForPassword
+    }
+    
+    func validatePhone(_ phone: String) -> Bool {
+        return phone.count == 11
+    }
+    
+    func validateAddress(_ address: String) -> Bool {
+        return !address.isEmpty
+    }
+    
+    func validateName(_ name: String) -> Bool {
+        return !name.isEmpty
+    }
+}
 
 class EmailValidator {
     static let shared = EmailValidator()
@@ -16,18 +60,18 @@ class EmailValidator {
     private init() {
         predicate = NSPredicate(format: "SELF MATCHES %@", regex)
     }
-    
+
     func validateEmail(_ email: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
         return predicate.evaluate(with: email)
     }
 }
 
 class PasswordValidator {
     static let shared = PasswordValidator()
+    let minimumSizeForPassword = 6
     
-    let minimumSize = 6
-    
-    func validatePassword(_ pass: String) -> Bool {
-        return pass.count >= minimumSize
+    func validatePassword(_ password: String) -> Bool {
+        return password.count >= minimumSizeForPassword
     }
 }
